252554629327ae5ce5c4059ab7509428f37dd46f
pkerekes <mail@kerekespeterjanos.hu>
2016-09-07 06:17:48 +0200

    draft note 2

--- iptables_geoip_filter.sh
+++ iptables_geoip_filter.sh
@@ -1,18 +1,11 @@
 
 wget https://sourceforge.net/projects/xtables-addons/files/Xtables-addons/2.11/xtables-addons-2.11.tar.xz -P /opt
-cd /opt
-tar xf xtables-addons-2.11.tar.xz 
-tar xf xtables-addons-2.11.tar.xz
-cd xtables-addons-2.11
-./configure 
-make
-make install
-ldconfig
+cd /opt && tar xf xtables-addons-2.11.tar.xz && cd xtables-addons-2.11
+./configure ;make ;make install ;ldconfig ;
 cd geoip/
 ./xt_geoip_dl
 ./xt_geoip_build GeoIPCountryWhois.csv
-mkdir -p /usr/share/xt_geoip
-cp -r {BE,LE} /usr/share/xt_geoip 
+mkdir -p /usr/share/xt_geoip && cp -r {BE,LE} /usr/share/xt_geoip 
 #iptables -I INPUT -m geoip --src-cc CN,US -j DROP 
 #iptables -nvL
 #netstat -tunp
