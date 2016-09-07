3abab13b923b15585ee04a633d8d7e27a6662b0a
pkerekes <mail@kerekespeterjanos.hu>
2016-09-07 06:24:44 +0200

    set vars

--- iptables_geoip_filter.sh
+++ iptables_geoip_filter.sh
@@ -1,6 +1,8 @@
-
-wget https://sourceforge.net/projects/xtables-addons/files/Xtables-addons/2.11/xtables-addons-2.11.tar.xz -P /opt
-cd /opt && tar xf xtables-addons-2.11.tar.xz && cd xtables-addons-2.11
+XT_VER="xtables-addons-2.11"
+XT_FIL="${XT_VER}.tar.xz"
+XT_DIR="/opt/$XT_VER"
+wget "https://sourceforge.net/projects/xtables-addons/files/Xtables-addons/2.11/${XT_FIL}" -P /opt
+cd /opt && tar xf "${XT_FIL}" && cd "${XT_DIR}"
 ./configure ;make ;make install ;ldconfig ;
 cd geoip/
 ./xt_geoip_dl
@@ -9,7 +11,9 @@ mkdir -p /usr/share/xt_geoip && cp -r {BE,LE} /usr/share/xt_geoip
 #iptables -I INPUT -m geoip --src-cc CN,US -j DROP 
 #iptables -nvL
 #netstat -tunp
-for COUNTRY in $(cat  /opt/xtables-addons-2.11/geoip/GeoIPCountryWhois.csv | awk -F'","' '{print $5}' | sort | uniq | grep -v DE);do iptables -I INPUT -m geoip --src-cc ${COUNTRY} -j DROP;done
+for COUNTRY in $(cat  /opt/xtables-addons-2.11/geoip/GeoIPCountryWhois.csv | awk -F'","' '{print $5}' | sort | uniq | grep -v DE);do 
+iptables -I INPUT -m geoip --src-cc ${COUNTRY} -j DROP;
+done
 #iptables -nvL | awk awk '$2>0{print}'
 
 
