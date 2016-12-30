b2355c9fa2c5082b1e52d3a85353cfab55774d03
pkerekes <mail@kerekespeterjanos.hu>
2016-12-30 18:39:27 +0100

    First init for Multi VM's

---
+++ vagran/multi-vm/vm-init.sh
@@ -0,0 +1,6 @@
+#Add user
+USERNAME='nemo'
+USERPASS='toor123'
+adduser $USERNAME --gecos "First Last,RoomNumber,WorkPhone,HomePhone" --disabled-password  && echo "$USERNAME:$USERPASS" | chpasswd
+echo "$USERNAME  ALL=(ALL:ALL) ALL" >> /etc/sudoers
+cp -rv /home/ubuntu/.ssh/ /home/${USERNAME}/ && chown -R nemo: /home/${USERNAME}/
